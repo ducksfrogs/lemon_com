20cd59b68c94ad52d063c3d7c04dbdd2b39ede2f
ducksfrogs <ma_yamaki@yahoo.com>
2022-01-25 06:22:15 +0900

    dd

--- future_sales/code/eda.py
+++ future_sales/code/eda.py
@@ -30,3 +30,15 @@ sales.date = sales.date.apply(lambda x:datetime.datetime.strptime(x, '%d.%m.%Y')
 monthly_sales = sales.groupby(["date_block_num", "shop_id", "item_id"])[
                 "date", "item_price", "item_cnt_day"].agg({"date":["min", "max"],
                                         "item_price": "mean", "item_cnt_day": "sum"})
+
+
+x = item.groupby(['item_category_id']).count()
+x = x.sort_values(by='item_id', ascending=False)
+x = x.iloc[0:10].reset_index()
+
+plt.figure(figsize=(8, 4))
+ax = sns.barplot(x.item_category_id, x.item_id, alpha=0.8)
+plt.title("Items per Category")
+plt.ylabel("# of items", fontsize=12)
+plt.xlabel("Category", fontsize=12)
+plt.show()
