01a34e01769d86e6427fcd437d448a9180d9df19
ducksfrogs <ma_yamaki@yahoo.com>
2022-01-26 06:06:22 +0900

    morning rou

--- future_sales/code/eda.py
+++ future_sales/code/eda.py
@@ -8,9 +8,10 @@ import seaborn as sns
 
 from statsmodels.tsa.arima_model import ARIMA
 from statsmodels.tsa.statespace.sarimax import SARIMAX
+import statsmodels.api as sm
 
 
-#settings
+# settings
 import warnings
 warnings.filterwarnings("ignore")
 
@@ -42,3 +43,23 @@ plt.title("Items per Category")
 plt.ylabel("# of items", fontsize=12)
 plt.xlabel("Category", fontsize=12)
 plt.show()
+
+
+# single series
+
+ts = sales.groupby(["date_block_num"])["item_cnt_day"].sum()
+ts.astype('float')
+plt.figure(figsize=(16, 8))
+plt.title('Total Sales of the company')
+plt.xlabel('Time')
+plt.ylabel('Sales')
+plt.plot(ts)
+
+plt.figure(figsize=(16, 6))
+plt.plot(ts.rolling(window=12, center=False).mean(), label='Rolling Mean')
+plt.plot(ts.rolling(window=12, center=False).std(), label="Rolling sd")
+plt.legend()
+
+
+res = sm.tsa.seasonal_decompose(ts.values, freq=12, model='multiplicative')
+fig = plt.plot()
